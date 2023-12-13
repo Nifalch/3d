f85e62d90c562f8e30754fcf6ae6e6fef1919168
Muhammed Nifal C H <90884960+Nifalch@users.noreply.github.com>
2023-12-13 17:25:18 +0530

    nifal

---
+++ src/components/Overlay/SectionDetails/sectionContentData.jsx
@@ -0,0 +1,21 @@
+// Components
+import TeamCarousel from "../TeamCarousel";
+
+// Sections content data
+const sectionsContentData = [
+  {
+    key: "about",
+    title: "About Me",
+    content: (
+      <p>
+       Its me Muhammed Nifal C H. I'm a 4th -year B.Tech Information Technology student at MEA Engineering College Perinthalmanna. I am an authority in WordPress and a computer enthusiast.I'm enthusiastic about the future and sure that I can have a constructive influence on it. I'm committed to using my knowledge and abilities to improve the lives of others.
+      </p>
+    ),
+  },
+  { key: "team", title: "Contact Me", content: <TeamCarousel /> },
+  {
+    
+  },
+];
+
+export default sectionsContentData;
---
+++ src/components/Overlay/TeamCarousel/teamData.js
@@ -0,0 +1,58 @@
+// Team data
+const teamData = [
+  {
+    name: "Muhammed Nifal C H",
+    image: "/images/nifal.jpg",
+    links: [
+      { title: "linkedin", link: "https://www.linkedin.com/in/muhammed-nifal-c-h" },
+      { title: "github", link: "https://github.com/Nifalch" },
+      {title: "twitter", link: ""},
+      { title: "portfolio", link: "https://nifalch.github.io/portfolio-website/" },
+    ],
+  },
+  {
+    name: "Muhammed Nifal C H",
+    image: "/images/nifal.jpg",
+    links: [
+      { title: "linkedin", link: "https://www.linkedin.com/in/muhammed-nifal-c-h" },
+      { title: "github", link: "https://github.com/Nifalch" },
+      {title: "twitter", link: ""},
+      { title: "portfolio", link: "https://nifalch.github.io/portfolio-website/" },
+    ],
+  },
+  {
+    name: "Muhammed Nifal C H",
+    Discrption: "Nifal is FaGoodread" ,
+    image: "/images/nifal.jpg",
+    links: [
+      { title: "linkedin", link: "https://www.linkedin.com/in/muhammed-nifal-c-h" },
+      { title: "github", link: "https://github.com/Nifalch" },
+      {title: "twitter", link: ""},
+      { title: "portfolio", link: "https://nifalch.github.io/portfolio-website/" },
+    ],
+  },
+  {
+    name: "Muhammed Nifal C H",
+    image: "/images/nifal.jpg",
+    links: [
+      { title: "linkedin", link: "https://www.linkedin.com/in/muhammed-nifal-c-h" },
+      { title: "github", link: "https://github.com/Nifalch" },
+      {title: "twitter", link: ""},
+      { title: "portfolio", link: "https://nifalch.github.io/portfolio-website/" },
+    ],
+  },
+  {
+    name: "Muhammed Nifal C H",
+    image: "/images/nifal.jpg",
+    links: [
+      { title: "linkedin", link: "https://www.linkedin.com/in/muhammed-nifal-c-h" },
+      { title: "github", link: "https://github.com/Nifalch" },
+      {title: "twitter", link: ""},
+      { title: "portfolio", link: "https://nifalch.github.io/portfolio-website/" },
+    ],
+  },
+ 
+  
+];
+
+export default teamData;
